a3321fd747b1581c8f8cc50566356ebfc15c1f2d
anteahee1400 <anteahee1400@naver.com>
2021-11-03 17:14:56 +0900

    train, test argparse 수정

--- logo_clf/test.py
+++ logo_clf/test.py
@@ -31,14 +31,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default=None, help="enter user config yaml path")
     parser.add_argument("--wandb", action="store_true", help="wandb")
-    parser.add_argument("--device", type=str, default="0", choices=["0", "1", "cpu"], help="device")
+    parser.add_argument("--device", type=str, default=None, choices=["0", "1", "cpu"], help="device")
+    parser.add_argument("--ckpt", type=str, default=None, help="checkpoint")
 
     args = parser.parse_args()
 
     default_config = read_yaml("config/default.yaml")
     config = update_config(default_config, args.config)
-    if args.device == 'cpu':
+    
+    if args.device is None:
+        pass
+    elif args.device == 'cpu':
         config['trainer'].pop('gpus')
     else:
         config['trainer']['gpus'] = int(args.device)
-    test(config, args.wandb)
+    
+    test(config, args.wandb, args.ckpt)
--- logo_clf/train.py
+++ logo_clf/train.py
@@ -34,14 +34,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default=None, help="enter user config yaml path")
     parser.add_argument("--wandb", action="store_true", help="wandb")
-    parser.add_argument("--device", type=str, default="0", choices=["0", "1", "cpu"], help="device")
+    parser.add_argument("--device", type=str, default=None, choices=["0", "1", "cpu"], help="device")
 
     args = parser.parse_args()
 
     default_config = read_yaml("config/default.yaml")
     config = update_config(default_config, args.config)
-    if args.device == 'cpu':
+    
+    if args.device is None:
+        pass
+    elif args.device == 'cpu':
         config['trainer'].pop('gpus')
     else:
         config['trainer']['gpus'] = int(args.device)
+
     train(config, args.wandb)
